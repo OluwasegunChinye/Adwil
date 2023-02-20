0abdb518c4555163d941ef221dc810d5f1516bf5
oluwasegunchinye <oluwasegunchinye@gmail.com>
2023-02-20 14:42:08 +0100

    minor

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -37,7 +37,7 @@ const OnboardingScreen = () => {
                             className="w-72 h-72 object-contain"
                         />
                     ),
-                    title: 'Over 400',
+                    title: 'Over 500+',
                     subtitle: 'Inspirational Praise and Worship Songs & Hymns',
                 },
                 {
